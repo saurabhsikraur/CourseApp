1e3da5ff4d35d27af8b8485fc5bf5d87b2a2b739
Learn Coding <90298313+LearnCodingOfficial@users.noreply.github.com>
2025-01-07 14:46:00 +0530

    user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -83,9 +83,6 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    if (!req.cookies.jwt) {
-      return res.status(401).json({ errors: "Kindly login first" });
-    }
     res.clearCookie("jwt");
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
