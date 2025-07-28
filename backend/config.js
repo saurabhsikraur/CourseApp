effcf1fc7cee0a8397fa7c8443e471a88f097f28
saurabhsikraur <saurabhsikraur@gmail.com>
2025-07-28 18:11:01 +0530

    Fix: removed hardcoded stripe secret and used env variable

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,7 @@ dotenv.config();
 
 const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
-const STRIPE_SECRET_KEY =
-  "sk_test_51QcX6kECbgbmkR5tJLgZ3ogh8BFo1cePgHQBpIWMKRiprvOMSej0A5Il8QvC0EqRTiiDQilTu2Oa4qcrXWvkS8Yq00cRaslSBO";
-
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY; 
 export default {
   JWT_USER_PASSWORD,
   JWT_ADMIN_PASSWORD,
