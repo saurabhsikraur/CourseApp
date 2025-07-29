b113d254fbcdd91cb1db5a6f649798b274d1f18d
saurabhsikraur <saurabhsikraur@gmail.com>
2025-07-29 19:40:12 +0530

    Fixed: utils.js file updated for frontend

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,2 +1,6 @@
 // export const BACKEND_URL = "https://courseapp-73af.onrender.com/api/v1";
-export const BACKEND_URL = "http://localhost:4001/api/v1";
+//export const BACKEND_URL = "http://localhost:4001/api/v1";
+
+export const BACKEND_URL="https://courseapp-pgw4.onrender.com/api/v1";
+
+// dummy change to force git to detect
